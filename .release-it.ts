d84464d600daf6b9bc256e2e0954b2266ad2316e
Rais <vdcoolzi@gmail.com>
2025-08-07 14:18:13 +0800

    ..

--- .release-it.ts
+++ .release-it.ts
@@ -1,11 +1,12 @@
 import type { Config } from "release-it";
 
 export default {
-  git: {
-    commitMessage: "tauri-plugin-macos-permissions-api v${version}",
-    tagName: "v${version}",
-  },
-  npm: {
-    publish: true,
-  },
+    git: {
+        commitMessage: "tauri-plugin-macos-permissions-api v${version}",
+        tagName: "v${version}",
+    },
+    npm: {
+        publish: true,
+    },
 } satisfies Config;
+
